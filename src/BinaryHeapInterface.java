3ef67b9404c6c8647ab6d5a0126336ba310d6f13
Pierlorenzo Peruzzo - Laptop <pierlorenzo.peruzzo@gmail.com>
2022-04-07 16:43:44 -0700

    Initialize MaxHeap

--- src/BinaryHeapInterface.java
+++ src/BinaryHeapInterface.java
@@ -1,10 +1,23 @@
 package src;
 
-public interface BinaryHeapInterface<T> {
+public interface BinaryHeapInterface<T extends Comparable<? super T>> {
+
+    public void add(T newEntry);
 
     public boolean canAdd();
 
     public int getParentIndex(int index);
 
     public int getChildIndex(int index);
+
+    public boolean isEmpty();
+
+    public int getSize();
+
+    public void clear();
+
+    public T removeRoot();
+
+    public T getRoot();
+    
 }
--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -1,6 +1,41 @@
 package src;
 
-public class MaxHeap<T> implements BinaryHeapInterface<T> {
+import org.junit.platform.engine.support.hierarchical.DefaultParallelExecutionConfigurationStrategy;
+
+public class MaxHeap<T extends Comparable<? super T>> implements BinaryHeapInterface<T> {
+
+    //#region Const
+
+    private static final int DEFAULT_CAPACITY = 25;
+
+    //#endregion
+
+    //#region Private Fields
+
+    private ResizeableArray<T> heap;
+    private int lastIndex;
+    private boolean initialized;
+
+    //#endregion
+
+    //#region Constructors
+
+    public MaxHeap(){
+        this(DEFAULT_CAPACITY);
+    }
+
+    public MaxHeap(int initialCapacity){
+        if (initialCapacity < DEFAULT_CAPACITY)
+            initialCapacity = DEFAULT_CAPACITY;
+
+        heap = new ResizeableArray(initialCapacity);
+        lastIndex = 0;
+        initialized = true;
+    }
+
+    //#endregion
+
+    //#region Implement BinaryHeapInterface
 
     @Override
     public boolean canAdd() {
@@ -20,4 +55,41 @@ public class MaxHeap<T> implements BinaryHeapInterface<T> {
         return 0;
     }
 
+    @Override
+    public void add(T newEntry) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public int getSize() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public T removeRoot() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public T getRoot() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    //#endregion
 }
